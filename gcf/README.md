899e6e862572039aca5a6b4be3c87ce98e89100d
Andreas Braumann <andreas@kaleido.ai>
2021-04-14 18:36:37 +0200

    - serverless function on google cloud

---
+++ gcf/README.md
@@ -0,0 +1,6 @@
+## Setup Google Cloud Functions
+https://cloud.google.com/functions/docs/first-nodejs
+
+1. Copy code of `index.js`and `package.json` into gloud console
+2. set a variable `REMOVE_BG_API_KEY`
+3. call the endpoint without API key in the header
---
+++ gcf/index.js
@@ -0,0 +1,102 @@
+const util = require('util');
+const FormData = require('form-data');
+const axios = require('axios');
+const busboy = require('async-busboy');
+const fs = require('fs');
+
+const Busboy = require("busboy");
+const parseFormData = (event) => {
+  return new Promise((resolve, reject) => {
+    const busboy = new Busboy({
+      headers: {
+        ...event.headers,
+        "content-type":
+          event.headers["Content-Type"] || event.headers["content-type"],
+      },
+    });
+    const result = {
+      files: [],
+    };
+
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+      file.on("data", (data) => {
+        result.files.push({
+          file: data,
+          fileName: filename,
+          contentType: mimetype,
+        });
+      });
+    });
+    busboy.on("field", (fieldname, value) => {
+      try {
+        result[fieldname] = JSON.parse(value);
+      } catch (err) {
+        result[fieldname] = value;
+      }
+    });
+    busboy.on("error", (error) => reject(`Parse error: ${error}`));
+    busboy.on("finish", () => {
+      event.body = result;
+      resolve(event);
+    });
+    busboy.write(event.body, event.isBase64Encoded ? "base64" : "binary");
+    busboy.end();
+  });
+};
+
+
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.removebg = async (req, res) => {
+ console.log("lambda");
+  try {
+    await parseFormData(req);
+    var form = new FormData();
+
+    for (var i = 0; i < req.body.files; ++i) {
+      var file = req.body.files[i];
+      console.log(file);
+    }
+    delete req.body.files;
+
+    Object.keys(req.body).forEach((key, index) => {
+      var val = req.body[key];
+      form.append(key, val.toString());
+      console.log(key, val.toString());
+    })
+
+    try {
+      var options = {
+        method: 'post',
+        url: 'https://api.remove.bg/v1.0/removebg',
+        headers: {
+          'accept': 'image/*',
+          'X-API-Key': process.env.REMOVE_BG_API_KEY,
+          'Content-Type': 'application/x-www-form-urlencoded',
+          ...form.getHeaders()
+        },
+        data: form,
+        responseType: "arraybuffer"
+      };
+
+      var axiosResponse = await axios(options);
+
+      res.status(axiosResponse.status).set('content-type', axiosResponse.headers['content-type']).send(axiosResponse.data);
+      
+
+      //var response = { statusCode: axiosResponse.status, body: axiosResponse.data, headers: {'content-type': axiosResponse.headers['content-type']} };
+      //return(response);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send(error.message);
+    }
+  } catch (error) {
+    console.log(err.message);
+    res.status(500).send(error.message);
+  }  
+};
